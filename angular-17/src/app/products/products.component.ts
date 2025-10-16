618cbc1ab503854fe8d20810f1c9ba8b3d0ab4af
Your Name <you@example.com>
2025-10-17 01:12:45 +0300

    product update cards

--- angular-17/src/app/products/products.component.ts
+++ angular-17/src/app/products/products.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Interface } from 'readline';
 
-export interface Product {
+export interface IProduct {
   name: string;
   price: number;
   img: string;
@@ -15,10 +15,15 @@ export interface Product {
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
-  Products: Product[] = [
-  { name: 'TV', price: 1000, img: 'tv.jpg' },
-  { name: 'Laptop', price: 1500, img: 'laptop.jpg' },
-  { name: 'Screen', price: 300, img: 'screen.jpg' }
+  Products: IProduct[] = [
+  { name: 'TV', price: 1000, img: 'assets/images/angular.png' },
+  { name: 'Laptop', price: 1500, img: 'assets/images/angular.png' },
+  { name: 'Screen', price: 300, img: 'assets/images/angular.png' }
 ];
+  Products2: object[]= [
+    {name: 'TV', price: 10000, img: 'tv.jpg'},
+    { name: 'Laptop', price: 1500, img: 'laptop.jpg' },
+    { name: 'Screen', price: 300, img: 'screen.jpg' },
+  ]
 
 }
